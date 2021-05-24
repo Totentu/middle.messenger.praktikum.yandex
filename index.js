41a2f298656f5bcfb8a28db8fa6e677bde6d1065
Ivan Efimov <efimov_il@sch2001.ru>
2021-05-24 17:05:45 +0300

    0005

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('static'));
- 
+app.use('/static', express.static(__dirname + '/static'));
+
 app.listen(PORT, function () {
     console.log(`Example app listening on port ${PORT}!`);
 });
